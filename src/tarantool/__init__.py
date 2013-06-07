94b7b395bf4900053498b22124db8ca35885ab10
Vlad Emelyanov <volshebnyi@gmail.com>
2013-06-07 17:23:56 +0400

    Updated to 0.3.1/ No more commits directly to master

--- src/tarantool/__init__.py
+++ src/tarantool/__init__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0301,W0105,W0401,W0614
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from tarantool.connection import Connection
 from tarantool.const import *
 from tarantool.error import *
 from tarantool.schema import *
 
+
 def connect(host="localhost", port=33013, schema=None):
     '''\
     Create a connection to the Tarantool server.
-    
+
     :param str host: Server hostname or IP-address
     :param int port: Server port
     :param schema: Data schema (see Developer guide and :class:`~tarantool.schema.Schema`)
@@ -20,10 +21,10 @@ def connect(host="localhost", port=33013, schema=None):
     :rtype: :class:`~tarantool.connection.Connection`
     :raise: `NetworkError`
     '''
-    
+
     return Connection(host, port,
                       socket_timeout=SOCKET_TIMEOUT,
                       reconnect_max_attempts=RECONNECT_MAX_ATTEMPTS,
                       reconnect_delay=RECONNECT_DELAY,
                       connect_now=True,
-                      schema = schema)
+                      schema=schema)
