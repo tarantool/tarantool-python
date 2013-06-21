tarantool/tarantool-python

commit c6df53755e4d18542c2634aff5d9830c69529464

Initial schema support

src/tarantool/connection.py
@@ -19,6 +19,7 @@ from tarantool.request import (
 from tarantool.space import Space
 from tarantool.const import *
 from tarantool.error import *
+from tarantool.schema import *
 
 
 
@@ -34,7 +35,8 @@ class Connection(object):
                  socket_timeout=SOCKET_TIMEOUT,
                  reconnect_max_attempts=RECONNECT_MAX_ATTEMPTS,
                  reconnect_delay=RECONNECT_DELAY,
-                 connect_now=True):
+                 connect_now=True,
+                 schema = None):
         '''\
         Initialize a connection to the server.
 
@@ -42,12 +44,18 @@ class Connection(object):
         :param int port: Server port
         :param bool connect_now: if True (default) than __init__() actually creates network connection.
                              if False than you have to call connect() manualy.
+        :param schema: Data schema (see Developer guide and :class:`~tarantool.schema.Schema`)
+        :type schema: :class:`~tarantool.schema.Schema` or dict
         '''
         self.host = host
         self.port = port
         self.socket_timeout = socket_timeout
         self.reconnect_delay = reconnect_delay
         self.reconnect_max_attempts = reconnect_max_attempts
+        if isinstance(schema, Schema):
+            self.schema = schema
+        else:
+            self.schema = Schema(schema)
         self._socket = None
         if connect_now:
             self.connect()
@@ -116,7 +124,7 @@ class Connection(object):
 
 
 
-    def _send_request_wo_reconnect(self, request, field_types=None):
+    def _send_request_wo_reconnect(self, request, space_name = None, field_defs = None, default_type = None):
         '''\
         :rtype: `Response` instance
 
@@ -129,7 +137,7 @@ class Connection(object):
             try:
                 self._socket.sendall(bytes(request))
                 header, body = self._read_response()
-                response = Response(header, body, field_types)
+                response = Response(self, header, body, space_name, field_defs, default_type)
             except socket.error as e:
                 raise NetworkError(e)
 
@@ -141,7 +149,7 @@ class Connection(object):
         raise DatabaseError(response.return_code, response.return_message)
 
 
-    def _send_request(self, request, field_types=None):
+    def _send_request(self, request, space_name = None, field_defs = None, default_type = None):
         '''\
         Send the request to the server through the socket.
         Return an instance of `Response` class.
@@ -162,7 +170,7 @@ class Connection(object):
                     self.connect()
                     connected = True
                     warn("Successfully reconnected", NetworkWarning)
-                response = self._send_request_wo_reconnect(request, field_types)
+                response = self._send_request_wo_reconnect(request, space_name, field_defs, default_type)
                 break
             except NetworkError as e:
                 if attempt > self.reconnect_max_attempts:
@@ -183,6 +191,13 @@ class Connection(object):
         :type args: list or tuple
         :param return_tuple: True indicates that it is required to return the inserted tuple back
         :type return_tuple: bool
+        :param field_defs: field definitions used for types conversion,
+               e.g. [('field0', tarantool.NUM), ('field1', tarantool.STR)]
+        :type field_defs: None or  [(name, type) or None]
+        :param default_type: None a default type used for result conversion, as defined in ``schema[space_no]['default_type']``
+        :type default_type: None or int
+        :param space_name: space number or name. A schema for the space will be used for type conversion.
+        :type space_name: None or int or str
 
         :rtype: `Response` instance
         '''
@@ -193,43 +208,45 @@ class Connection(object):
         if isinstance(args[0], (list, tuple)):
             args = args[0]
 
-        # Check if 'field_types' keyword argument is passed
-        field_types = kwargs.get("field_types", None)
+        # Check if 'field_defs' and 'default_type' keyword arguments are passed
+        field_defs = kwargs.get("field_defs", None)
+        default_type = kwargs.get("default_type", None)
+        space_name = kwargs.get("space_name", None)
 
-        request = RequestCall(func_name, args, return_tuple=True)
-        response = self._send_request(request, field_types=field_types)
+        request = RequestCall(self, func_name, args, return_tuple=True)
+        response = self._send_request(request, space_name = space_name,
+                                      field_defs = field_defs,
+                                      default_type = default_type)
         return response
 
 
-    def insert(self, space_no, values, return_tuple=False, field_types=None):
+    def insert(self, space_name, values, return_tuple=False):
         '''\
         Execute INSERT request.
-        Insert single record into a space `space_no`.
+        Insert single record into a space `space_name`.
 
-        :param int space_no: space id to insert a record
-        :type space_no: int
+        :param int space_name: space id to insert a record
+        :type space_name: int or str
         :param values: record to be inserted. The tuple must contain only scalar (integer or strings) values
         :type values: tuple
         :param return_tuple: True indicates that it is required to return the inserted tuple back
         :type return_tuple: bool
-        :param field_types: Data types to be used for type conversion.
-        :type field_types: tuple
 
         :rtype: `Response` instance
         '''
         assert isinstance(values, tuple)
 
-        request = RequestInsert(space_no, values, return_tuple)
-        return self._send_request(request, field_types=field_types)
+        request = RequestInsert(self, space_name, values, return_tuple)
+        return self._send_request(request, space_name)
 
 
-    def delete(self, space_no, key, return_tuple=False, field_types=None):
+    def delete(self, space_name, key, return_tuple=False):
         '''\
         Execute DELETE request.
         Delete single record identified by `key` (using primary index).
 
-        :param space_no: space id to delete a record
-        :type space_no: int
+        :param space_name: space number or name to delete a record
+        :type space_name: int or name
         :param key: key that identifies a record
         :type key: int or str
         :param return_tuple: indicates that it is required to return the deleted tuple back
@@ -237,21 +254,21 @@ class Connection(object):
 
         :rtype: `Response` instance
         '''
-        assert isinstance(key, (int, basestring))
+        assert isinstance(key, (int, long, basestring))
 
-        request = RequestDelete(space_no, key, return_tuple)
-        return self._send_request(request, field_types=field_types)
+        request = RequestDelete(self, space_name, key, return_tuple)
+        return self._send_request(request, space_name)
 
 
-    def update(self, space_no, key, op_list, return_tuple=False, field_types=None):
+    def update(self, space_name, key, op_list, return_tuple=False):
         '''\
         Execute UPDATE request.
         Update single record identified by `key` (using primary index).
 
         List of operations allows to update individual fields.
 
-        :param space_no: space id to update a record
-        :type space_no: int
+        :param space_name: space number or name to update a record
+        :type space_name: int or str
         :param key: key that identifies a record
         :type key: int or str
         :param op_list: list of operations. Each operation is tuple of three values
@@ -263,8 +280,8 @@ class Connection(object):
         '''
         assert isinstance(key, (int, basestring))
 
-        request = RequestUpdate(space_no, key, op_list, return_tuple)
-        return self._send_request(request, field_types=field_types)
+        request = RequestUpdate(space_name, key, op_list, return_tuple)
+        return self._send_request(request, space_name)
 
 
     def ping(self):
@@ -284,14 +301,14 @@ class Connection(object):
         return t1 - t0
 
 
-    def _select(self, space_no, index_no, values, offset=0, limit=0xffffffff, field_types=None):
+    def _select(self, space_name, index_name, values, offset=0, limit=0xffffffff):
         '''\
         Low level version of select() method.
 
-        :param space_no: space id to select data
-        :type space_no: int
-        :param index_no: index id to use
-        :type index_no: int
+        :param space_name: space number of name to select data
+        :type space_name: int or str
+        :param index_name: index id to use
+        :type index_name: int or str
         :param values: list of values to search over the index
         :type values: list of tuples
         :param offset: offset in the resulting tuple set
@@ -307,18 +324,18 @@ class Connection(object):
         assert len(values) != 0
         assert isinstance(values[0], (list, tuple))
 
-        request = RequestSelect(space_no, index_no, values, offset, limit)
-        response = self._send_request(request, field_types=field_types)
+        request = RequestSelect(self, space_name, index_name, values, offset, limit)
+        response = self._send_request(request, space_name)
         return response
 
 
-    def select(self, space_no, values, **kwargs):
+    def select(self, space_name, values, **kwargs):
         '''\
         Execute SELECT request.
         Select and retrieve data from the database.
 
-        :param space_no: specifies which space to query
-        :type space_no: int
+        :param space_name: specifies which space to query
+        :type space_name: int or str
         :param values: list of values to search over the index
         :type values: list of tuples
         :param index: specifies which index to use (default is **0** which means that the **primary index** will be used)
@@ -349,16 +366,15 @@ class Connection(object):
         # Initialize arguments and its defaults from **kwargs
         offset = kwargs.get("offset", 0)
         limit = kwargs.get("limit", 0xffffffff)
-        field_types = kwargs.get("field_types", None)
         index = kwargs.get("index", 0)
 
         # Perform smart type cheching (scalar / list of scalars / list of tuples)
-        if isinstance(values, (int, basestring)): # scalar
+        if isinstance(values, (int, long, basestring)): # scalar
             # This request is looking for one single record
             values = [(values, )]
         elif isinstance(values, (list, tuple, set, frozenset)):
             assert len(values) > 0
-            if isinstance(values[0], (int, basestring)): # list of scalars
+            if isinstance(values[0], (int, long, basestring)): # list of scalars
                 # This request is looking for several records using single-valued index
                 # Ex: select(space_no, index_no, [1, 2, 3])
                 # Transform a list of scalar values to a list of tuples
@@ -369,20 +385,20 @@ class Connection(object):
             else:
                 raise ValueError("Invalid value type, expected one of scalar (int or str) / list of scalars / list of tuples ")
 
-        return self._select(space_no, index, values, offset, limit, field_types=field_types)
+        return self._select(space_name, index, values, offset, limit)
 
 
-    def space(self, space_no, field_types=None):
+    def space(self, space_name):
         '''\
         Create `Space` instance for particular space
 
         `Space` instance encapsulates the identifier of the space and provides more convenient syntax
         for accessing the database space.
 
-        :param space_no: identifier of the space
-        :type space_no: int
+        :param space_name: identifier of the space
+        :type space_name: int or str
 
         :rtype: `Space` instance
         '''
-        return Space(self, space_no, field_types)
+        return Space(self, space_name)
 

src/tarantool/request.py
@@ -26,10 +26,9 @@ class Request(object):
         )
     )
 
-    def __init__(self):
+    def __init__(self, conn):
         self._bytes = None
-        raise NotImplementedError("Abstract method must be overridden")
-
+        self.conn = conn
 
     def __bytes__(self):
         return self._bytes
@@ -39,25 +38,6 @@ class Request(object):
     @classmethod
     def header(cls, body_length):
         return struct_LLL.pack(cls.request_type, body_length, 0)
-
-
-    @staticmethod
-    def pack_int(value):
-        '''\
-        Pack integer field
-        <field> ::= <int32_varint><data>
-
-        :param value: integer value to be packed
-        :type value: int
-
-        :return: packed value
-        :rtype: bytes
-        '''
-        if __debug__:
-            if not isinstance(value, int):
-                raise TypeError("Invalid argument type '%s'. Only 'int' expected"%type(value).__name__)
-        return struct_BL.pack(4, value)
-
 
     @classmethod
     def pack_int_base128(cls, value):
@@ -100,47 +80,12 @@ class Request(object):
         raise OverflowError("Number too large to be packed")
 
 
-    @classmethod
-    def pack_str(cls, value):
-        '''\
-        Pack string field
-        <field> ::= <int32_varint><data>
-
-        :param value: string to be packed
-        :type value: bytes or str
-
-        :return: packed value
-        :rtype: bytes
-        '''
-        if __debug__:
-            if not isinstance(value, basestring):
-                raise TypeError("Invalid argument type '%s', 'str' expected"%type(value).__name__)
-        value_len_packed = cls.pack_int_base128(len(value))
+    def pack_field(self, value):
+        value_len_packed = Request.pack_int_base128(len(value))
         return struct.pack("<%ds%ds"%(len(value_len_packed), len(value)), value_len_packed,  value)
 
 
-    @classmethod
-    def pack_field(cls, value):
-        '''\
-        Pack single field (string or integer value)
-        <field> ::= <int32_varint><data>
-
-        :param value: value to be packed
-        :type value: bytes, str or int
-
-        :return: packed value
-        :rtype: bytes
-        '''
-        if isinstance(value, basestring):
-            return cls.pack_str(value)
-        elif isinstance(value, (int, long)):
-            return cls.pack_int(value)
-        else:
-            raise TypeError("Invalid argument type '%s'. Only 'str' or 'int' expected"%type(value).__name__)
-
-
-    @classmethod
-    def pack_tuple(cls, values):
+    def pack_fields(self, packed_values):
         '''\
         Pack tuple of values
         <tuple> ::= <cardinality><field>+
@@ -151,15 +96,41 @@ class Request(object):
         :return: packed tuple
         :rtype: bytes
         '''
-        assert isinstance(values, (tuple, list))
-        cardinality = struct_L.pack(len(values))
-        packed_items = [cls.pack_field(v) for v in values]
-        packed_items.insert(0, cardinality)
+        assert isinstance(packed_values, (tuple, list))
+        cardinality = struct_L.pack(len(packed_values))
+        packed_items = []
+        packed_items.append(cardinality)
+        for value in packed_values:
+            packed_items.append(self.pack_field(value))
         return b"".join(packed_items)
 
 
+    def pack_tuple(self, values, space_no = None):
+        '''\
+        Pack tuple of values
+        <tuple> ::= <cardinality><field>+
+
+        :param value: tuple to be packed
+        :type value: tuple of scalar values (bytes, str or int)
+
+        :return: packed tuple
+        :rtype: bytes
+        '''
+        return self.pack_fields(self.conn.schema.pack_values(values, space_no))
 
 
+    def pack_key_tuple(self, values, space_no = None, index_no = None):
+        '''\
+        Pack key tuple
+        <tuple> ::= <cardinality><field>+
+
+        :param value: key tuple to be packed
+        :type value: tuple of scalar values (bytes, str or int)
+
+        :return: packed tuple
+        :rtype: bytes
+        '''
+        return self.pack_fields(self.conn.schema.pack_key(values, space_no, index_no))
 
 class RequestInsert(Request):
     '''\
@@ -173,15 +144,18 @@ class RequestInsert(Request):
     '''
     request_type = REQUEST_TYPE_INSERT
 
-    def __init__(self, space_no, values, return_tuple):    # pylint: disable=W0231
+    def __init__(self, conn, space_name, values, return_tuple): # pylint: disable=W0231
         '''\
         '''
+        super(RequestInsert, self).__init__(conn)
+
         assert isinstance(values, (tuple, list))
         flags = 1 if return_tuple else 0
 
+        space_no = self.conn.schema.space_no(space_name)
         request_body = \
             struct_LL.pack(space_no, flags) + \
-            self.pack_tuple(values)
+            self.pack_tuple(values, space_no)
 
         self._bytes = self.header(len(request_body)) + request_body
 
@@ -200,17 +174,18 @@ class RequestDelete(Request):
     '''
     request_type = REQUEST_TYPE_DELETE
 
-    def __init__(self, space_no, key, return_tuple):    # pylint: disable=W0231
+    def __init__(self, conn, space_name, key, return_tuple):    # pylint: disable=W0231
         '''
         '''
+        super(RequestDelete, self).__init__(conn)
         flags = 1 if return_tuple else 0
 
+        space_no = self.conn.schema.space_no(space_name)
         request_body = \
             struct_LL.pack(space_no, flags) + \
-            self.pack_tuple((key,))
+            self.pack_key_tuple((key,), space_no, 0)
 
         self._bytes = self.header(len(request_body)) + request_body
-
 
 
 class RequestSelect(Request):
@@ -230,13 +205,15 @@ class RequestSelect(Request):
     '''
     request_type = REQUEST_TYPE_SELECT
 
-    def __init__(self, space_no, index_no, tuple_list, offset, limit):    # pylint: disable=W0231
-
+    def __init__(self, conn, space_name, index_name, tuple_list, offset, limit):    # pylint: disable=W0231
+        super(RequestSelect, self).__init__(conn)
         assert isinstance(tuple_list, (list, tuple))
 
+        space_no = self.conn.schema.space_no(space_name)
+        index_no = self.conn.schema.index_no(space_no, index_name)
         request_body = \
             struct_LLLLL.pack(space_no, index_no, offset, limit, len(tuple_list)) + \
-            b"".join([self.pack_tuple(t) for t in tuple_list])
+            b"".join([self.pack_key_tuple(t, space_no, index_no) for t in tuple_list])
 
         self._bytes = self.header(len(request_body)) + request_body
 
@@ -256,18 +233,19 @@ class RequestUpdate(Request):
 
     request_type = REQUEST_TYPE_UPDATE
 
-    def __init__(self, space_no, key, op_list, return_tuple):    # pylint: disable=W0231
+    def __init__(self, conn, space_name, key, op_list, return_tuple):    # pylint: disable=W0231
+        super(RequestUpdate, self).__init__(conn)
         flags = 1 if return_tuple else 0
         assert isinstance(key, (int, basestring))
 
+        space_no = self.conn.schema.space_no(space_name)
         request_body = \
             struct_LL.pack(space_no, flags) + \
-            self.pack_tuple((key,)) + \
+            self.pack_key_tuple((key,), space_no, 0) + \
             struct_L.pack(len(op_list)) +\
             self.pack_operations(op_list)
 
         self._bytes = self.header(len(request_body)) + request_body
-
 
     @classmethod
     def pack_operations(cls, op_list):
@@ -300,13 +278,14 @@ class RequestCall(Request):
     '''
     request_type = REQUEST_TYPE_CALL
 
-    def __init__(self, proc_name, args, return_tuple):    # pylint: disable=W0231
+    def __init__(self, conn, proc_name, args, return_tuple):    # pylint: disable=W0231
+        super(RequestCall, self).__init__(conn)
         flags = 1 if return_tuple else 0
         assert isinstance(args, (list, tuple))
 
         request_body = \
             struct_L.pack(flags) + \
             self.pack_field(proc_name) +\
-            self.pack_tuple(args)
+            self.pack_tuple([k for k in args])
 
         self._bytes = self.header(len(request_body)) + request_body

src/tarantool/__init__.py
@@ -6,14 +6,17 @@ __version__ = "0.3.0"
 from tarantool.connection import Connection
 from tarantool.const import *
 from tarantool.error import *
+from tarantool.schema import *
 
-def connect(host="localhost", port=33013):
+def connect(host="localhost", port=33013, schema=None):
     '''\
     Create a connection to the Tarantool server.
     
     :param str host: Server hostname or IP-address
     :param int port: Server port
-    
+    :param schema: Data schema (see Developer guide and :class:`~tarantool.schema.Schema`)
+    :type schema: :class:`~tarantool.schema.Schema` or dict
+
     :rtype: :class:`~tarantool.connection.Connection`
     :raise: `NetworkError`
     '''
@@ -22,4 +25,5 @@ def connect(host="localhost", port=33013):
                       socket_timeout=SOCKET_TIMEOUT,
                       reconnect_max_attempts=RECONNECT_MAX_ATTEMPTS,
                       reconnect_delay=RECONNECT_DELAY,
-                      connect_now=True)
+                      connect_now=True,
+                      schema = schema)

src/tarantool/const.py
@@ -18,6 +18,7 @@ struct_LLL = struct.Struct("<LLL")
 struct_LLLL = struct.Struct("<LLLL")
 struct_LLLLL = struct.Struct("<LLLLL")
 struct_Q = struct.Struct("<Q")
+struct_BQ = struct.Struct("<BQ")
 
 
 REQUEST_TYPE_CALL = 22

src/tarantool/response.py
@@ -79,7 +79,7 @@ class Response(list):
     packet received from the server.
     '''
 
-    def __init__(self, header, body, field_types=None):
+    def __init__(self, conn, header, body, space_name = None, field_defs = None, default_type = None):
         '''\
         Create an instance of `Response` using data received from the server.
 
@@ -102,7 +102,13 @@ class Response(list):
         self._return_code = None
         self._return_message = None
         self._rowcount = None
-        self.field_types = field_types
+        self.conn = conn
+        if space_name is not None:
+            self.space_no = conn.schema.space_no(space_name)
+        else:
+            self.space_no = None
+        self.field_defs = field_defs
+        self.default_type = default_type
 
         # Unpack header
         self._request_type, self._body_length, self._request_id  = struct_LLL.unpack(header)
@@ -178,7 +184,7 @@ class Response(list):
 
         # In case of an error unpack the body as an error message
         if self._return_code != 0:
-            self._return_message = unicode(buff.value, "utf8", "replace")
+            self._return_message = unicode(buff[4:-1], "utf8", "replace")
             if self._completion_status == 2:
                 raise DatabaseError(self._return_code, self._return_message)
 
@@ -199,10 +205,9 @@ class Response(list):
                 tuple_size = struct.unpack_from("<L", buff, offset)[0] + 4
                 tuple_data = struct.unpack_from("<%ds"%(tuple_size), buff, offset+4)[0]
                 tuple_value = self._unpack_tuple(tuple_data)
-                if self.field_types:
-                    self.append(self._cast_tuple(tuple_value))
-                else:
-                    self.append(tuple_value)
+                tuple_value = self.conn.schema.unpack_values(tuple_value,
+                                self.space_no, self.field_defs, self.default_type)
+                self.append(tuple_value)
 
                 offset = offset + tuple_size + 4    # This '4' is a size of <size> attribute
 
@@ -254,49 +259,6 @@ class Response(list):
         The error message returned by the server in case of :attr:`return_code` is non-zero.
         '''
         return self._return_message
-
-
-    @staticmethod
-    def _cast_field(cast_to, value):
-        '''\
-        Convert field type from raw bytes to native python type
-
-        :param cast_to: native python type to cast to
-        :type cast_to: a type object (one of bytes, int, unicode (str for py3k))
-        :param value: raw value from the database
-        :type value: bytes
-
-        :return: converted value
-        :rtype: value of native python type (one of bytes, int, unicode (str for py3k))
-        '''
-
-        if cast_to in (int, unicode):
-            return cast_to(value)
-        elif cast_to in (any, bytes):
-            return value
-        else:
-            raise TypeError("Invalid field type %s"%(cast_to))
-
-
-    def _cast_tuple(self, values):
-        '''\
-        Convert values of the tuple from raw bytes to native python types
-
-        :param values: tuple of the raw database values
-        :type value: tuple of bytes
-
-        :return: converted tuple value
-        :rtype: value of native python types (bytes, int, unicode (or str for py3k))
-        '''
-        result = []
-        for i, value in enumerate(values):
-            if i < len(self.field_types):
-                result.append(self._cast_field(self.field_types[i], value))
-            else:
-                result.append(self._cast_field(self.field_types[-1], value))
-
-        return tuple(result)
-
 
     def __repr__(self):
         '''\

commit e5947749c4034e2cbc3c18538343e59ba9bc7798

Fix conn.update() broken by c6df53755e4d18542c2634aff5d9830c69529464

src/tarantool/connection.py
@@ -273,7 +273,7 @@ class Connection(object):
         '''
         assert isinstance(key, (int, basestring))
 
-        request = RequestUpdate(space_name, key, op_list, return_tuple)
+        request = RequestUpdate(self, space_name, key, op_list, return_tuple)
         return self._send_request(request, space_name)
 
 

src/tarantool/request.py
@@ -247,8 +247,7 @@ class RequestUpdate(Request):
 
         self._bytes = self.header(len(request_body)) + request_body
 
-    @classmethod
-    def pack_operations(cls, op_list):
+    def pack_operations(self, op_list):
         result = []
         for op in op_list:
             try:
@@ -260,7 +259,8 @@ class RequestUpdate(Request):
             except KeyError:
                 raise ValueError("Invalid operaction symbol '%s', expected one of %s"\
                                 %(op_symbol, ', '.join(["'%s'"%c for c in sorted(UPDATE_OPERATION_CODE.keys())])))
-            data = b"".join([struct_LB.pack(field_no, op_code), cls.pack_field(op_arg)])
+            op_arg = self.conn.schema.pack_value(op_arg)
+            data = b"".join([struct_LB.pack(field_no, op_code), self.pack_field(op_arg)])
             result.append(data)
         return b"".join(result)
 