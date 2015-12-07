2f961c904d6b6ffff280ea63170f2290486f7c10
Sulverus <sulverus@gmail.com>
2015-12-07 17:46:36 +0300

    Tests enabled

---
+++ test.sh
@@ -0,0 +1,6 @@
+curl http://tarantool.org/dist/public.key | sudo apt-key add -
+echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+sudo apt-get update > /dev/null
+sudo apt-get -q -y install tarantool
+pip install -r requirements.txt
+python setup.py test
