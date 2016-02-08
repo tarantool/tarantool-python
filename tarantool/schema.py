fffdaa78630d4d0cd13793dbb87e5ba8cb49bd16
bigbes <bigbes@gmail.com>
2016-02-08 17:30:16 +0300

    Add backwards compatibility (If we run tarantool 1.6.4 snapshot on tarantool 1.6.6 or more)

--- tarantool/schema.py
+++ tarantool/schema.py
@@ -7,21 +7,26 @@ It is a Tarantool schema description.
 
 import six
 
-from tarantool.error import SchemaError
+from tarantool.error import SchemaError, DatabaseError
 import tarantool.const as const
 
 
 class SchemaIndex(object):
-    def __init__(self, array, space):
-        self.iid = array[1]
-        self.name = array[2]
+    def __init__(self, index_row, space):
+        self.iid = index_row[1]
+        self.name = index_row[2]
         if isinstance(self.name, bytes):
             self.name = self.name.decode()
-        self.index = array[3]
-        self.unique = array[4]
+        self.index = index_row[3]
+        self.unique = index_row[4]
         self.parts = []
-        for i in range(array[5]):
-            self.parts.append((array[5 + 1 + i * 2], array[5 + 2 + i * 2]))
+        if isinstance(index_row[5], (list, tuple)):
+            for k, v in index_row[5]:
+                self.parts.append(k, v)
+        else:
+            for i in range(index_row[5]):
+                self.parts.append((index_row[5 + 1 + i * 2], index_row[5 + 2 + i * 2]))
+            sel
         self.space = space
         self.space.indexes[self.iid] = self
         if self.name:
@@ -34,10 +39,10 @@ class SchemaIndex(object):
 
 
 class SchemaSpace(object):
-    def __init__(self, array, schema):
-        self.sid = array[0]
-        self.arity = array[1]
-        self.name = array[2]
+    def __init__(self, space_row, schema):
+        self.sid = space_row[0]
+        self.arity = space_row[1]
+        self.name = space_row[2]
         if isinstance(self.name, bytes):
             self.name = self.name.decode()
         self.indexes = {}
@@ -66,15 +71,22 @@ class Schema(object):
                   if isinstance(space, six.string_types)
                   else const.INDEX_SPACE_PRIMARY)
 
-        array = self.con.select(const.SPACE_SPACE, space, index=_index)
-        if len(array) > 1:
-            raise SchemaError('Some strange output from server: \n' + array)
-        elif len(array) == 0 or not len(array[0]):
+        space_row = None
+        try:
+            space_row = self.con.select(const.SPACE_VSPACE, space, index=_index)
+        except DatabaseError as e:
+            if e.args[0] != 36:
+                raise
+        if space_row is None:
+            space_row = self.con.select(const.SPACE_SPACE, space, index=_index)
+        if len(space_row) > 1:
+            raise SchemaError('Some strange output from server: \n' + space_row)
+        elif len(space_row) == 0 or not len(space_row[0]):
             temp_name = ('name' if isinstance(space, six.string_types) else 'id')
             raise SchemaError(
                 "There's no space with {1} '{0}'".format(space, temp_name))
-        array = array[0]
-        return SchemaSpace(array, self.schema)
+        space_row = space_row[0]
+        return SchemaSpace(space_row, self.schema)
 
     def get_index(self, space, index):
         _space = self.get_space(space)
@@ -86,17 +98,26 @@ class Schema(object):
                   if isinstance(index, six.string_types)
                   else const.INDEX_INDEX_PRIMARY)
 
-        array = self.con.select(const.SPACE_INDEX, [_space.sid, index],
+        index_row = None
+        try:
+            index_row = self.con.select(const.SPACE_VSPACE, [_space.sid, index],
                                 index=_index)
-        if len(array) > 1:
-            raise SchemaError('Some strange output from server: \n' + array)
-        elif len(array) == 0 or not len(array[0]):
+        except DatabaseError as e:
+            if e.args[0] != 36:
+                raise
+        if index_row is None:
+            index_row = self.con.select(const.SPACE_SPACE, [_space.sid, index],
+                                index=_index)
+
+        if len(index_row) > 1:
+            raise SchemaError('Some strange output from server: \n' + index_row)
+        elif len(index_row) == 0 or not len(index_row[0]):
             temp_name = ('name' if isinstance(index, six.string_types) else 'id')
             raise SchemaError(
                 "There's no index with {2} '{0}' in space '{1}'".format(
                     index, _space.name, temp_name))
-        array = array[0]
-        return SchemaIndex(array, _space)
+        index_row = index_row[0]
+        return SchemaIndex(index_row, _space)
 
     def flush(self):
         self.schema.clear()
