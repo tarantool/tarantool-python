450cc3db0ba6ebfad6ec7e9df3f5b9a3f24d8277
Roman Tsisyk <roman@tsisyk.com>
2017-02-27 15:05:22 +0300

    Travis CI: update to the latest version of .travis.yml
    * Switch to PackPack for packages
    * Use `packagecloud` deployment instead of travis.sh script
    * Enable email notifications
    * Add Fedora 25, Ubuntu Yakkety
    * Remove Ubuntu Wily (EOL)

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl http://tarantool.org/dist/public.key | sudo apt-key add -
 echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
 sudo apt-get update > /dev/null
