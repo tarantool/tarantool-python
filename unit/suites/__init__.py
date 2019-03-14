145531d7e7cc1a3d938d836ebc27904db3525203
Alexander Turenko <alexander.turenko@tarantool.org>
2019-03-14 18:54:25 +0300

    test: add test cases for reconnections
    Fixes #125.

--- unit/suites/__init__.py
+++ unit/suites/__init__.py
@@ -7,8 +7,10 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 from .test_schema import TestSuite_Schema
 from .test_dml import TestSuite_Request
 from .test_protocol import TestSuite_Protocol
+from .test_reconnect import TestSuite_Reconnect
 
-test_cases = (TestSuite_Schema, TestSuite_Request, TestSuite_Protocol)
+test_cases = (TestSuite_Schema, TestSuite_Request, TestSuite_Protocol,
+              TestSuite_Reconnect)
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
