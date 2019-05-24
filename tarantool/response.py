bce4b88454ba2cc586d41bcd929a55496d6ca070
Oleg Babin <babinoleg@mail.ru>
2019-05-24 10:48:03 +0300

    Fix deprecation warning re import from collections
     | /usr/local/lib/python3.7/site-packages/tarantool/response.py:25
     |   /usr/local/lib/python3.7/site-packages/tarantool/response.py:25:
     |   DeprecationWarning: Using or importing the ABCs from 'collections'
     |   instead of from 'collections.abc' is deprecated, and in 3.8 it will
     |   stop working
     |     class Response(collections.Sequence):

--- tarantool/response.py
+++ tarantool/response.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0301,W0105,W0401,W0614
 
-import collections
+try:
+    # Python 3.3+
+    from collections.abc import Sequence
+except ImportError:
+    # Python 2
+    from collections import Sequence
 
 import json
 import msgpack
@@ -22,7 +27,7 @@ from tarantool.error import (
 )
 
 
-class Response(collections.Sequence):
+class Response(Sequence):
     '''
     Represents a single response from the server in compliance with the
     Tarantool protocol.
