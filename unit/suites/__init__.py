a9f00b3cc5e3aa36ef9ccb1f020cf5850a4fdc8f
Alexander Turenko <alexander.turenko@tarantool.org>
2020-08-27 18:30:00 +0300

    test: schema reload with different connection encodings

--- unit/suites/__init__.py
+++ unit/suites/__init__.py
@@ -4,14 +4,17 @@ import unittest
 __tmp = os.getcwd()
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-from .test_schema import TestSuite_Schema
+from .test_schema import TestSuite_Schema_UnicodeConnection
+from .test_schema import TestSuite_Schema_BinaryConnection
 from .test_dml import TestSuite_Request
 from .test_protocol import TestSuite_Protocol
 from .test_reconnect import TestSuite_Reconnect
 from .test_mesh import TestSuite_Mesh
 
-test_cases = (TestSuite_Schema, TestSuite_Request, TestSuite_Protocol,
-              TestSuite_Reconnect, TestSuite_Mesh)
+test_cases = (TestSuite_Schema_UnicodeConnection,
+              TestSuite_Schema_BinaryConnection,
+              TestSuite_Request, TestSuite_Protocol, TestSuite_Reconnect,
+              TestSuite_Mesh)
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
