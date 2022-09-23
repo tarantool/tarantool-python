fa17761faf1c15dc14706b72d4ad354af619630e
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2022-09-23 15:03:58 +0300

    doc: update AUTHORS
    Part of #67

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tarantool python client library'
-copyright = u'2011, Konstantin Cherkasoff'
+copyright = u'2011-2022, tarantool-python AUTHORS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -190,7 +190,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Tarantoolpythonclientlibrary.tex', u'Tarantool python client library Documentation',
-   u'Konstantin Cherkasoff', 'manual'),
+   u'tarantool-python AUTHORS', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,7 +220,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'tarantoolpythonclientlibrary', u'Tarantool python client library Documentation',
-     [u'Konstantin Cherkasoff'], 1)
+     [u'tarantool-python AUTHORS'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -234,7 +234,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Tarantoolpythonclientlibrary', u'Tarantool python client library Documentation',
-   u'Konstantin Cherkasoff', 'Tarantoolpythonclientlibrary', 'One line description of project.',
+   u'tarantool-python AUTHORS', 'Tarantoolpythonclientlibrary', 'One line description of project.',
    'Miscellaneous'),
 ]
 
