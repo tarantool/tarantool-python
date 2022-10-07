a49942e33b4fe11e29cb44d9511a7e062d852046
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2022-10-07 13:21:09 +0300

    doc: use sphinxdoc HTML theme
    Default 'classic' theme [1] was used by Python 2 documentation. It's not
    well-suited for modern resolutions and it's sidebar width isn't
    configurable, which is crucial for modules API ToC correct display.
    
    1. https://www.sphinx-doc.org/en/master/usage/theming.html
    
    Part of #67

--- doc/conf.py
+++ doc/conf.py
@@ -93,16 +93,16 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-html_style = 'tarantool.css'
+#html_style = 'style.css'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'sidebarwidth': '30%'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -126,7 +126,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
