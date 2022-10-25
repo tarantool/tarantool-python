0ed510b79bd02cba1c630e23115a09592cd46945
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2022-10-25 15:55:52 +0300

    do it

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,22 +4,20 @@ set -euo pipefail
 
 rpm_name='python3-tarantool'
 
-version=$(python3 setup.py --version)
-
-
 echo "Build spec..."
 
 rpm_deps='python3-msgpack>=1.0.3,python3-pandas,python3-pytz'
 
-SETUPTOOLS_SCM_PRETEND_VERSION=$version python3 setup.py bdist_rpm --spec-only --requires="$rpm_deps"
+SETUPTOOLS_SCM_DEBUG=1 python3 setup.py bdist_rpm --spec-only --requires="$rpm_deps"
 mv dist/tarantool.spec $HOME/rpmbuild/SPECS/$rpm_name.spec
 sed -i $HOME/rpmbuild/SPECS/$rpm_name.spec -e 's/%define name tarantool/%define name '$rpm_name'/'
 
 echo "Build sources..."
 
-SETUPTOOLS_SCM_PRETEND_VERSION=$version python3 setup.py sdist
+python3 setup.py sdist
 
 regex='s/^dist\/tarantool-\(.*\).tar.gz$/\1/'
+version=$(find dist/tarantool-*.tar.gz | sed $regex)
 
 tar -xvzf dist/tarantool-$version.tar.gz
 rm dist/tarantool-$version.tar.gz
