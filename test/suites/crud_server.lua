f1bb57c604baa50d818ec9f8d550a8fa9e51c507
Ilya Grishnov <ilya.grishnov@tarantool.org>
2023-01-12 12:16:48 +0300

    test: fixes the moduleless error of crud instance
    In crud tests, if the necessary rocks dependencies are missing,
    an error message is now displayed. Prior to this fix,
    an error in importing missing models caused the tests to freez forever.
    
    Part of #205

--- test/suites/crud_server.lua
+++ test/suites/crud_server.lua
@@ -1,7 +1,58 @@
 #!/usr/bin/env tarantool
 
-local crud = require('crud')
-local vshard = require('vshard')
+local function configure_crud_instance(primary_listen, crud, vshard)
+    box.schema.create_space(
+    'tester', {
+    format = {
+        {name = 'id', type = 'unsigned'},
+        {name = 'bucket_id', type = 'unsigned'},
+        {name = 'name', type = 'string'},
+    }
+    })
+    box.space.tester:create_index('primary_index', {
+        parts = {
+            {field = 1, type = 'unsigned'},
+        },
+    })
+    box.space.tester:create_index('bucket_id', {
+        parts = {
+            {field = 2, type = 'unsigned'},
+        },
+        unique = false,
+    })
+
+    -- Setup vshard.
+    _G.vshard = vshard
+    box.once('guest', function()
+        box.schema.user.grant('guest', 'super')
+    end)
+    local uri = 'guest@0.0.0.0:' .. primary_listen
+    local cfg = {
+        bucket_count = 300,
+        sharding = {
+            [box.info().cluster.uuid] = {
+                replicas = {
+                    [box.info().uuid] = {
+                        uri = uri,
+                        name = 'storage',
+                        master = true,
+                    },
+                },
+            },
+        },
+    }
+    vshard.storage.cfg(cfg, box.info().uuid)
+    vshard.router.cfg(cfg)
+    vshard.router.bootstrap()
+
+    -- Initialize crud.
+    crud.init_storage()
+    crud.init_router()
+    crud.cfg{stats = true}
+end
+
+local crud_imported, crud = pcall(require, 'crud')
+local vshard_imported, vshard = pcall(require, 'vshard')
 
 local admin_listen = os.getenv("ADMIN")
 local primary_listen = os.getenv("LISTEN")
@@ -18,51 +69,17 @@ box.schema.user.grant(
     'read,write,execute',
     'universe'
 )
-box.schema.create_space(
- 'tester', {
-  format = {
-    {name = 'id', type = 'unsigned'},
-    {name = 'bucket_id', type = 'unsigned'},
-    {name = 'name', type = 'string'},
-  }
-})
-box.space.tester:create_index('primary_index', {
-    parts = {
-        {field = 1, type = 'unsigned'},
-    },
-})
-box.space.tester:create_index('bucket_id', {
-    parts = {
-        {field = 2, type = 'unsigned'},
-    },
-    unique = false,
-})
 
--- Setup vshard.
-_G.vshard = vshard
-box.once('guest', function()
-    box.schema.user.grant('guest', 'super')
-end)
-local uri = 'guest@0.0.0.0:' .. primary_listen
-local cfg = {
-    bucket_count = 300,
-    sharding = {
-        [box.info().cluster.uuid] = {
-            replicas = {
-                [box.info().uuid] = {
-                    uri = uri,
-                    name = 'storage',
-                    master = true,
-                },
-            },
-        },
-    },
-}
-vshard.storage.cfg(cfg, box.info().uuid)
-vshard.router.cfg(cfg)
-vshard.router.bootstrap()
-
--- Initialize crud.
-crud.init_storage()
-crud.init_router()
-crud.cfg{stats = true}
+if crud_imported == false or vshard_imported == false then
+    -- Set flag for unittest.
+    _G['ROCKS_IMPORT_FAIL'] = true
+    local fail_msg = 'The crud/vshard modules are not detected, ' ..
+                     'installation via rocks install is required ' ..
+                     'for CRUD testing purposes. You can use ' ..
+                     '<tarantoolctl rocks install crud> or ' ..
+                     '<tt rocks install crud> to install modules'
+    -- The print output will be captured in the logs.
+    print(fail_msg)
+else
+    configure_crud_instance(primary_listen, crud, vshard)
+end
