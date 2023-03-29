34c87cbb78998edf7b6e035212311647e2a7b151
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-29 14:33:27 +0300

    internal: move validate script to bash
    validate_timezones is a prt of gen-timezones script, but it is easy to
    treat it as a part of the package (pylint do that too).
    
    Part of #270

--- tarantool/msgpack_ext/types/timezones/gen-timezones.sh
+++ tarantool/msgpack_ext/types/timezones/gen-timezones.sh
@@ -70,4 +70,17 @@ echo "}" >> ${DST_FILE}
 
 rm timezones.h
 
-python validate_timezones.py
+python <<EOF
+import pytz
+from timezones import timezoneToIndex, timezoneAbbrevInfo
+
+if __name__ != '__main__':
+    raise RuntimeError('Import not expected')
+
+for timezone in timezoneToIndex.keys():
+    if timezone in pytz.all_timezones:
+        continue
+
+    if not timezone in timezoneAbbrevInfo:
+        raise KeyError(f'Unknown Tarantool timezone {timezone}')
+EOF
