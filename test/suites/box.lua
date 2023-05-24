ef8cfd94f4f0b1f7f211ad017600c3b77a035317
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-05-24 18:41:03 +0300

    test: fix compatibility with Tarantool master
    New Tarantool 3.0 patch introduces a breaking change [1]. This patch
    adds a compatibility layer to test instance.
    
    It was decided to explicitly set SEQSCAN in our tests and change session
    options for DBAPI tests.
    
    1. https://github.com/tarantool/tarantool/pull/8602

--- test/suites/box.lua
+++ test/suites/box.lua
@@ -1,10 +1,16 @@
 #!/usr/bin/env tarantool
 
+local is_compat, compat = pcall(require, "compat")
 local os = require('os')
 
 local admin_listen = os.getenv("ADMIN")
 local primary_listen = os.getenv("LISTEN")
 local auth_type = os.getenv("AUTH_TYPE")
+
+local SQL_SEQ_SCAN_DEFAULT = os.getenv("SQL_SEQ_SCAN_DEFAULT")
+if is_compat and SQL_SEQ_SCAN_DEFAULT then
+    compat.sql_seq_scan_default = SQL_SEQ_SCAN_DEFAULT
+end
 
 require('console').listen(admin_listen)
 box.cfg{
